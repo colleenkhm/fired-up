7c3ec94cc562bd0669a4df62dbfe75ba53d61b13
Colleen Murray <colleenkhm@gmail.com>
2022-12-09 00:27:49 -0600

    player component and basic canvas styles

--- player.js
+++ player.js
@@ -1,5 +1,18 @@
 class Player {
     constructor(game){
         this.game = game;
+        this.width = 100;
+        this.height = 91.3;
+        this.x = 0;
+        this.y = this.game.height - this.height;
+        this.image = document.getElementById('player');
+    }
+    update() {
+
+    }
+    draw(context) {
+        context.fillStyle = 'red';
+        context.fillRect(this.x, this.y, this.width, this.height);
+        context.drawImage(this.image, sx, sy, sw, sh, this.x, this.y, this.width, this.height);
     }
 }
