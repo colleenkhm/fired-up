b0ede6d196c02f3c52753a1785961f02d650e5d8
Colleen Murray <colleenkhm@gmail.com>
2022-12-09 21:33:14 -0600

    basic movement functions, adding keys

--- main.js
+++ main.js
@@ -12,9 +12,10 @@ window.addEventListener('load', function() {
             this.width = width;
             this.height = height;
             this.player = new Player(this);
+            this.input = new InputHandler();
         }
         update(){
-            this.player.update();
+            this.player.update(this.input.keys);
         }
         draw(context){
             this.player.draw(context);
