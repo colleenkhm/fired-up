03ca9fb67d402993f628364fc6fec4cf1c923c2c
Colleen Murray <colleenkhm@gmail.com>
2022-12-19 22:51:29 -0600

    debug toggle and collision detection

--- player.js
+++ player.js
@@ -27,6 +27,7 @@ export class Player {
     this.currentState.enter();
   }
   update(input, deltaTime) {
+    this.checkCollision();
     this.currentState.handleInput(input);
     //horizontal movement
     this.x += this.speed;
@@ -55,6 +56,7 @@ export class Player {
     }
   }
   draw(context) {
+    if (this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height);
     context.drawImage(
       this.image,
       this.frameX * this.width,
@@ -75,5 +77,21 @@ export class Player {
     this.game.speed = this.game.maxSpeed * speed;
     this.currentState.enter();
     console.log(this.currentState);
+  }
+  checkCollision() {
+    this.game.enemies.forEach(enemy => {
+      if (
+        enemy.x < this.x + this.width &&
+        enemy.x + enemy.width > this.x &&
+        enemy.y < this.y + this.height &&
+        enemy.y + enemy.height > this.y
+      ) {
+        // collision detected
+        enemy.markedForDeletion = true;
+        this.game.score++;
+      } else {
+        // no collision
+      }
+    })
   }
 }
